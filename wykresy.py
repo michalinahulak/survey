9e991510188e575589352d85a3d8e04bf6565cf7
Michalina Hulak <michalina.hulak@op.pl>
2024-08-23 12:42:08 +0200

    Update wykresy.py

--- wykresy.py
+++ wykresy.py
@@ -97,7 +97,7 @@ Oceń, czy artykuły z podobnych tematów (np. artykuły sportowe) znajdują si�
 # Wyświetlenie filmu
 video_html = f"""
     <div style="display: flex; justify-content: left;">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/8QDNFvJfY7Y?si=kgfPpsn5NcrZ39ep&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="1280" height="688" src="https://www.youtube.com/embed/Qs8XsIo4aHI" title="Instrukcja wypełniania ankiety" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 """
 
